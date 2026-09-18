efebcb0580b72125924a5011de7a233c725c96c4
mauro <maurosoliz505@gmail.com>
2026-09-18 00:53:03 -0300

    Initial commit

---
+++ src/main/java/com/page/faq/FaqPageApplication.java
@@ -0,0 +1,13 @@
+package com.page.faq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaqPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaqPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/page/faq/FaqPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.page.faq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaqPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
